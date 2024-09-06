c02f5bcdef44b0e765d816ac9223eee4df028d6c
leetaehun <taehun1231@naver.com>
2024-09-06 11:16:13 +0900

    read 수정

--- 0906CRUD.py
+++ 0906CRUD.py
@@ -1,14 +1,34 @@
-from read import readMembers
-from read import readMemberId
+from read import readMember, readMemberId
 
+
+def menu_selection():
+    print("(C)reate, (R)ead, (U)pdate, (D)elete")
+    menu = input("메뉴 입력: ").upper() 
+
+    if menu == "C":
+        print()
+        # Create 관련 로직 추가
+    elif menu == "R":
+         print("(A)ll, (S)elect")
+         choice = input("선택: ").upper() 
+
+         if choice == "A":
+            readMember()
+         elif choice == "S":
+            try:
+                member_id = int(input("조회할 회원의 ID를 입력하세요: "))
+                readMemberId(member_id)
+            except ValueError:
+                print("올바른 숫자를 입력하세요.")
+    elif menu == "U":
+        print()
+        # Update 관련 로직 추가
+    elif menu == "D":
+        print()
+        # Delete 관련 로직 추가
+    else:
+        print("잘못된 입력입니다. 다시 시도하세요.")
 
 
 if __name__ == "__main__":
-    try:
-        # 사용자로부터 회원 ID 입력받기
-        user_input = int(input("조회할 회원의 ID를 입력하세요: "))
-        # 입력받은 ID로 회원 정보 조회
-        read_member_by_id(user_input)
-    except ValueError:
-        print("올바른 숫자를 입력하세요.")
-    readMembers()
+    menu_selection()
--- read.py
+++ read.py
@@ -1,3 +1,5 @@
+from createmember import addMember
+
 members = [
     {"id": 1, "name": "Alice", "birth": "1990-01-01"},
     {"id": 2, "name": "Bob", "birth": "1985-05-12"},
@@ -5,7 +7,7 @@ members = [
 ]
 
 #전체 회원 조회
-def readMembers():
+def readMember():
     print("회원 목록:")
     for member in members:
         print(f"ID: {member['id']}, Name: {member['name']}, Birth: {member['birth']}")
@@ -13,7 +15,7 @@ def readMembers():
 #선택 회원 조회
 def readMemberId(member_id):
     for member in members:
-        if member['member_id'] == member_id:
-            print(f"ID: {member['member_id']}, Name: {member['name']}, Birth: {member['birth']}")
+        if member['id'] == member_id:
+            print(f"ID: {member['id']}, Name: {member['name']}, Birth: {member['birth']}")
             return
     print(f"회원 ID {member_id}를 찾을 수 없습니다.")
