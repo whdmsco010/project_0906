613a43dde321173a2633f6a9eaf0e61e7c1c266f
puppyzzoa <sy_2283@naver.com>
2024-09-06 10:35:08 +0900

    회원 추가 함수

---
+++ createmember.py
@@ -0,0 +1,11 @@
+members = []
+
+def addMember(name, birth):
+    id = len(members) + 1  
+    member = {
+        'id': id,
+        'name': name,
+        'birth': birth,
+    }
+    members.append(member)
+    print(f"Member added: {member}")
